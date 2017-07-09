f093fcb6e59de8f9ce64e21e4f0fd2b3f271046b
fablecode <fablecode@users.noreply.github.com>
2017-07-09 11:43:29 +0100

    Urlhelper added.

---
+++ src/wikia/Helper/UrlHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace wikia.Helper
+{
+    public class UrlHelper
+    {
+        public static string GenerateUrl(string url, NameValueCollection parameters)
+        {
+            var isValidUrl = Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+            if (!isValidUrl)
+                throw new ArgumentException($"{nameof(GenerateUrl)} method: {nameof(url)} parameter is invalid.");
+
+            var urlBuilder = new UriBuilder(url);
+
+            if (parameters != null && parameters.Count > 0)
+            {
+                IEnumerable<string> segments = 
+                (
+                    from key in parameters.AllKeys
+                    from value in parameters.GetValues(key)
+                    select $"{Uri.EscapeUriString(key)}={Uri.EscapeUriString(value)}"
+                )
+                .ToArray();
+
+                urlBuilder.Query = string.Join("&", segments);
+            }
+
+            return urlBuilder.Uri.AbsoluteUri;
+        }
+    }
+}
---
+++ tests/unit/wikia.unit.tests/UrlHelperTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using FluentAssertions;
+using NUnit.Framework;
+using wikia.Helper;
+
+namespace wikia.unit.tests
+{
+    [TestFixture]
+    public class UrlHelperTests
+    {
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("      ")]
+        [TestCase("dfsfs/dsfsfsf")]
+        [TestCase("file://awesome file.exe")]
+        [TestCase("ldap://lightweightdirectoryaccess/user")]
+        public void Given_An_Invalid_Url_Should_Throw_ArgumentException(string url)
+        {
+            // Arrange
+            
+            // Act
+            Action act = () => UrlHelper.GenerateUrl(url, default(NameValueCollection));
+
+            // Assert
+            act
+                .ShouldThrow<ArgumentException>();
+        }
+
+        [TestCaseSource(nameof(GenerateUrlTestData))]
+        public void Given_A_Valid_Url_And_QueryStringParameters_Should_Generate_Url(string url, NameValueCollection parameters, string expected)
+        {
+            // Arrange
+
+            // Act
+            var result = UrlHelper.GenerateUrl(url, parameters);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        private static IEnumerable<TestCaseData> GenerateUrlTestData
+        {
+            get
+            {
+                yield return new TestCaseData
+                (
+                    "http://yugioh.wikia.com/api/v1/Articles/List", new NameValueCollection
+                    {
+                        {"category", "Card_Tips"},
+                        { "limit", "200"}
+                    }, 
+                    "http://yugioh.wikia.com/api/v1/Articles/List?category=Card_Tips&limit=200"
+                );
+                yield return new TestCaseData
+                (
+                    "http://yugioh.wikia.com/api/v1/Articles/List", new NameValueCollection
+                    {
+                        {"category", "Card_Tips"},
+                        { "limit", "200"},
+                        {"offset", "page|414d415a4f4e455353205350454c4c434153544552|93782"}
+                    },
+                    "http://yugioh.wikia.com/api/v1/Articles/List?category=Card_Tips&limit=200&offset=page%7C414d415a4f4e455353205350454c4c434153544552%7C93782"
+                );
+            }
+        }
+    }
+}
