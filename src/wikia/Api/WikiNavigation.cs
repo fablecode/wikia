a96aa5b4ebd9a7e6e2bb00b5b03aa36b223dea42
fablecode <fablecode@users.noreply.github.com>
2017-07-16 17:08:28 +0100

    Wikia WikiNavigation added. Navigation data about specific wiki.

--- src/wikia/Api/WikiNavigation.cs
+++ src/wikia/Api/WikiNavigation.cs
@@ -1,40 +1,41 @@
 ﻿using System.Threading.Tasks;
 using wikia.Configuration;
 using wikia.Helper;
-using wikia.Models.Mercury;
+using wikia.Models;
 
 namespace wikia.Api
 {
-    public class WikiMercury : IWikiMercury
+    public class WikiNavigation : IWikiNavigation
     {
         private readonly IWikiaHttpClient _wikiaHttpClient;
         private readonly string _wikiApiUrl;
-        private readonly string _wikiVariablesUrl = "Mercury/WikiVariables";
+        private readonly string _navigationLinks = "Navigation/Data";
 
-        public WikiMercury(string domainUrl)
+        public WikiNavigation(string domainUrl)
             : this(domainUrl, WikiaSettings.RelativeApiUrl)
         {
 
         }
 
-        public WikiMercury(string domainUrl, string relativeApiUrl)
+        public WikiNavigation(string domainUrl, string relativeApiUrl)
             : this(domainUrl, relativeApiUrl, new WikiaHttpClient())
         {
 
         }
 
-        public WikiMercury(string domainUrl, string relativeApiUrl, IWikiaHttpClient wikiaHttpClient)
+        public WikiNavigation(string domainUrl, string relativeApiUrl, IWikiaHttpClient wikiaHttpClient)
         {
             _wikiApiUrl = UrlHelper.GenerateApiUrl(domainUrl, relativeApiUrl);
             _wikiaHttpClient = wikiaHttpClient;
         }
 
-        public async Task<WikiDataContainer> WikiVariables()
+        public async Task<NavigationResultSet> NavigationLinks()
         {
-            var requestUrl = UrlHelper.GenerateApiUrl(_wikiApiUrl, _wikiVariablesUrl);
+            var requestUrl = UrlHelper.GenerateApiUrl(_wikiApiUrl, _navigationLinks);
             var json = await _wikiaHttpClient.Get(requestUrl);
 
-            return JsonHelper.Deserialize<WikiDataContainer>(json);
+            return JsonHelper.Deserialize<NavigationResultSet>(json);
         }
+
     }
 }
---
+++ tests/integration/wikia.integration.tests/Navigation/NavigationLinksTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using wikia.Api;
+using wikia.integration.tests.WikiaArticleTests;
+
+namespace wikia.integration.tests.Navigation
+{
+    [TestFixture]
+    public class NavigationLinksTests
+    {
+        [TestCaseSource(typeof(WikiaArticleTestData), nameof(WikiaArticleTestData.WikiUrlsTestData))]
+        public async Task Given_A_DomainUrl__Should_Retrieve_WikiVariables(string domainUrl)
+        {
+            // Arrange
+            var sut = new WikiNavigation(domainUrl);
+
+            // Act
+            var result = await sut.NavigationLinks();
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+    }
+}
