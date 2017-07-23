943e87a1add8067b1bfdb06f0c58222ad097d780
fablecode <fablecode@users.noreply.github.com>
2017-07-23 11:52:54 +0100

    AssemblyInfo description changed.

--- src/wikia/Properties/AssemblyInfo.cs
+++ src/wikia/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Wikia")]
-[assembly: AssemblyDescription("Wikia is a C# library that makes it easy to access Wiki website data.")]
+[assembly: AssemblyDescription("Wikia is a C# library that makes it easy to access Wiki data.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("wikia")]
